1601db9fe01555c402ae5dc5fc3f7b5e77629821
Aleksey <120055748+AleksejVoronkin@users.noreply.github.com>
2024-04-15 19:30:18 +0300

    Add files via upload

---
+++ JStest/Spring6145/Seminar03/example3_sem3_hometask/src/main/java/ru/gb/example3_sem3_hometask/services/UserService.java
@@ -0,0 +1,21 @@
+package ru.gb.example3_sem3_hometask.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.gb.example3_sem3_hometask.domain.User;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private NotificationService notificationService;
+
+    public User createUser(String name, int age, String email) {
+        User user = new User();
+        user.setName(name);
+        user.setAge(age);
+        user.setEmail(email);
+        notificationService.notifyUser(user);
+        return user;
+    }
+}
